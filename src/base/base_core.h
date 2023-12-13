83ca117db3fcc78a6bb403a4c1c41af34e59f455
Cr0wn24 <hampan2003@live.se>
2023-12-13 19:45:16 +0100

    Context cracking formatting

--- src/base/base_core.h
+++ src/base/base_core.h
@@ -33,89 +33,89 @@
 #	endif
 
 #elif defined(_MSC_VER)
-#		define COMPILER_CL 1
+#	define COMPILER_CL 1
 
-# 	if defined(_WIN32)
-#  		define OS_WINDOWS 1
-#		else
-#			error missing OS detection
-#		endif
+#	if defined(_WIN32)
+#		define OS_WINDOWS 1
+#	else
+#		error missing OS detection
+#	endif
 
-# 	if defined(_M_AMD64)
-#			define ARCH_X64 1
-#		elif defined(_M_IX86)
-#			define ARCH_X86 1
-#		elif defined(_M_ARM)
-#			define ARCH_ARM
-#		else
-#			error missing ARCH detection
-#		endif
+#	if defined(_M_AMD64)
+#		define ARCH_X64 1
+#	elif defined(_M_IX86)
+#		define ARCH_X86 1
+#	elif defined(_M_ARM)
+#		define ARCH_ARM
+#	else
+#		error missing ARCH detection
+#	endif
 
 #elif defined(__GNUC__)
-#		define COMPILER_GCC 1
+#	define COMPILER_GCC 1
 
-#		if defined(_WIN32)
-#			define OS_WINDOWS 1
-#		elif defined(__gnu_linux__)
-#			define OS_LINUX 1
-#		elif defined(__APPLE__) && defined(__MACH__)
-#			define OS_MAC 1
-#		else
-#			error missing OS detection
-#		endif
+#	if defined(_WIN32)
+#		define OS_WINDOWS 1
+#	elif defined(__gnu_linux__)
+#		define OS_LINUX 1
+#	elif defined(__APPLE__) && defined(__MACH__)
+#		define OS_MAC 1
+#	else
+#		error missing OS detection
+#	endif
 
-# 	if defined(__amd64__)
-#			define ARCH_X64 1
-#		elif defined(__i386__)
-#			define ARCH_X86 1
-#		elif defined(__arm__)
-#			define ARCH_ARM
-#		elif defined(__aarch64__)
-#			define ARCH_ARM64
-#		else
-#			error missing ARCH detection
-#		endif
+#	if defined(__amd64__)
+#		define ARCH_X64 1
+#	elif defined(__i386__)
+#		define ARCH_X86 1
+#	elif defined(__arm__)
+#		define ARCH_ARM
+#	elif defined(__aarch64__)
+#		define ARCH_ARM64
+#	else
+#		error missing ARCH detection
+#endif
 
 #endif
 
 #if !defined(COMPILER_CL)
-#		define COMPILER_CL 0
+#	define COMPILER_CL 0
 #endif
 
 #if !defined(COMPILER_CLANG)
-#		define COMPILER_CLANG 0
+#	define COMPILER_CLANG 0
 #endif
 
 #if !defined(COMPILER_GCC)
-#		define COMPILER_GCC 0
+#	define COMPILER_GCC 0
 #endif
 
 #if !defined(OS_WINDOWS)
-#		define OS_WINDOWS 0
+#	define OS_WINDOWS 0
 #endif
 
 #if !defined(OS_LINUX)
-#		define OS_LINUX 0
+#	define OS_LINUX 0
 #endif
 
 #if !defined(OS_MAC)
-#		define OS_MAC 0
+#	define OS_MAC 0
 #endif
 
 #if !defined(ARCH_X64)
-#		define ARCH_X64 0
+#	define ARCH_X64 0
 #endif
 
 #if !defined(ARCH_X86)
-#		define ARCH_X86 0
+#	define ARCH_X86 0
 #endif
 
 #if !defined(ARCH_ARM)
-#		define ARCH_ARM 0
+#	define ARCH_ARM 0
 #endif
 
 #if !defined(ARCH_ARM64)
-#		define ARCH_ARM64 0
+#	define ARCH_ARM64 0
 #endif
 
 #define check_null(x) ((x) == 0)
