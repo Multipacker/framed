3aa9b42a97f062b4ae6d7f07b7350d02af1d9e65
Multipacker <simon.renhult@hotmail.se>
2023-12-14 08:17:36 +0100

    Dev: Link with libm.so on clang.

--- build_clang.sh
+++ build_clang.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+mkdir -p build
+
 src_files="src/main.c"
 errors="-Werror -Wall -Wno-missing-braces -Wno-newline-eof -Wno-unused-variable -Wno-unused-function -Wconversion"
 compiler_flags="-Isrc -g -fsanitize=address"
+linker_flags=""
 
-clang $src_files $compiler_flags $errors -o build/out
+clang $src_files $compiler_flags $linker_flags $errors -o build/out
