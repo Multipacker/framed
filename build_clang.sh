8be3ccd94602cad496b8e169244e7d8e2122d7d2
Multipacker <simon.renhult@hotmail.se>
2023-12-14 20:12:46 +0100

    Dev: Update gcc and clang build scripts to support multiple targets.

--- build_clang.sh
+++ build_clang.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
+mode=${1:-debug}
+
 mkdir -p build
 
 src_files="src/linux_test.c"
 errors="-Werror -Wall -Wno-missing-braces -Wno-newline-eof -Wno-unused-variable -Wno-unused-function -Wconversion"
-compiler_flags="-Isrc -g -fsanitize=address"
+common_flags="-Isrc -Ivendor -o build/out"
 linker_flags=""
 
-clang $src_files $compiler_flags $linker_flags $errors -o build/out
+if [ "$mode" == "debug" ]; then
+	compiler_flags="-g -fsanitize=address"
+elif [ "$mode" == "optimized"]; then
+	compiler_flags="-g -O3 -fsanitize=address"
+elif [ "$mode" == "release"]; then
+	compiler_flags=""
+else
+	echo "ERROR: Unknown build type."
+	exit 1
+fi
+
+clang $src_files $errors $common_flags $compiler_flags $linker_flags
--- build_gcc.sh
+++ build_gcc.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
+mode=${1:-debug}
+
 mkdir -p build
 
 src_files="src/main.c"
 errors="-Werror -Wall -Wno-missing-braces -Wno-unused-variable -Wno-unused-function"
-compiler_flags="-Isrc -g -fsanitize=address"
+common_flags="-Isrc -Ivendor -o build/out"
 linker_flags="-lm"
 
-gcc $src_files $compiler_flags $linker_flags $errors -o build/out
+if [ "$mode" == "debug" ]; then
+	compiler_flags="-g -fsanitize=address"
+elif [ "$mode" == "optimized"]; then
+	compiler_flags="-g -fsanitize=address"
+elif [ "$mode" == "release"]; then
+	compiler_flags=""
+else
+	echo "ERROR: Unknown build type."
+	exit 1
+fi
+
+gcc $src_files $errors $common_flags $compiler_flags $linker_flags
