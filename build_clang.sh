99334fd1306692a5aa9dc2ae35a2c9e5a01ac6f3
Simon Renhult <simon.renhult@hotmail.se>
2023-12-14 07:45:23 +0100

    Dev: Added more warnings to clang.

--- build_clang.sh
+++ build_clang.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-src_files="src/clang_compiler_test.c"
-errors="-Werror -Wall -Wno-missing-braces -Wno-newline-eof -Wno-unused-variable -Wno-unused-function"
+src_files="src/main.c"
+errors="-Werror -Wall -Wno-missing-braces -Wno-newline-eof -Wno-unused-variable -Wno-unused-function -Wconversion"
 compiler_flags="-Isrc -g -fsanitize=address"
 
 clang $src_files $compiler_flags $errors -o build/out
