6e8d53e2f09c492d2c5af4653348a57952975c7a
Simon Renhult <simon.renhult@hotmail.se>
2023-12-15 20:20:20 +0100

    Dev: Changed to using the new build macros for clang and gcc.

--- build_clang.sh
+++ build_clang.sh
@@ -4,17 +4,17 @@ mode=${1:-debug}
 
 mkdir -p build
 
-src_files="src/linux_test.c"
+src_files="src/main.c"
 errors="-Werror -Wall -Wno-missing-braces -Wno-newline-eof -Wno-unused-variable -Wno-unused-function -Wconversion"
 common_flags="-Isrc -Ivendor -o build/out"
 linker_flags=""
 
 if [ "$mode" == "debug" ]; then
-	compiler_flags="-g -fsanitize=address -DBUILD_MODE=0"
+	compiler_flags="-g -fsanitize=address -DBUILD_MODE_DEBUG=1"
 elif [ "$mode" == "optimized"]; then
-	compiler_flags="-g -O3 -fsanitize=address -DBUILD_MODE=1"
+	compiler_flags="-g -O3 -fsanitize=address -DBUILD_MODE_OPTIMIZED=1"
 elif [ "$mode" == "release"]; then
-	compiler_flags="-DBUILD_MODE=2"
+	compiler_flags="-DBUILD_MODE_RELEASE=1"
 else
 	echo "ERROR: Unknown build type."
 	exit 1
--- build_gcc.sh
+++ build_gcc.sh
@@ -10,11 +10,11 @@ common_flags="-Isrc -Ivendor -o build/out"
 linker_flags="-lm"
 
 if [ "$mode" == "debug" ]; then
-	compiler_flags="-g -fsanitize=address -DBUILD_MODE=0"
+	compiler_flags="-g -fsanitize=address -DBUILD_MODE_DEBUG=1"
 elif [ "$mode" == "optimized"]; then
-	compiler_flags="-g -fsanitize=address -DBUILD_MODE=1"
+	compiler_flags="-g -fsanitize=address -DBUILD_MODE_OPTIMIZED=1"
 elif [ "$mode" == "release"]; then
-	compiler_flags="-DBUILD_MODE=2"
+	compiler_flags="-DBUILD_MODE_RELEASE=1"
 else
 	echo "ERROR: Unknown build type."
 	exit 1
