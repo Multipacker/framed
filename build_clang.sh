a2ab70a4329582682e651af08fb7ad80902c0c21
Multipacker <simon.renhult@hotmail.se>
2023-12-16 17:09:11 +0100

    Typo: Added some critical *spaces* that were missing in the build scripts.

--- build_clang.sh
+++ build_clang.sh
@@ -11,9 +11,9 @@ linker_flags="-lSDL2"
 
 if [ "$mode" == "debug" ]; then
 	compiler_flags="-g -fsanitize=address -DBUILD_MODE_DEBUG=1"
-elif [ "$mode" == "optimized"]; then
+elif [ "$mode" == "optimized" ]; then
 	compiler_flags="-g -O3 -fsanitize=address -DBUILD_MODE_OPTIMIZED=1"
-elif [ "$mode" == "release"]; then
+elif [ "$mode" == "release" ]; then
 	compiler_flags="-DBUILD_MODE_RELEASE=1"
 else
 	echo "ERROR: Unknown build type."
