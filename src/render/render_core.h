88eeb360711766f5c91a6679d1b4fd6ed9ef6336
Cr0wn24 <hampan2003@live.se>
2023-12-16 13:45:35 +0100

    Dev: Renamed RenderStats to R_RenderStats

--- src/render/render_core.h
+++ src/render/render_core.h
@@ -5,6 +5,7 @@
 // - Textures
 // - Text rendering
 // - Lines
+// - Icons
 
 typedef struct R_RectInstance R_RectInstance;
 struct R_RectInstance
@@ -17,8 +18,8 @@ struct R_RectInstance
 	F32     border_thickness;
 };
 
-typedef struct RenderStats RenderStats;
-struct RenderStats
+typedef struct R_RenderStats R_RenderStats;
+struct R_RenderStats
 {
 	U64 draw_time;
 	U64 scissor_count;
@@ -48,6 +49,7 @@ struct R_RectParams
 // TODO(hampus): Test performance with/without passing by pointer
 #define render_rect(renderer, min, max, ...)    render_rect_(renderer, min, max, &(R_RectParams) { .color = v4f32(1, 1, 1, 1), __VA_ARGS__ })
 #define render_circle(renderer, center, r, ...) render_rect_(renderer, v2f32_sub_f32(center, r), v2f32_add_f32(center, r), &(R_RectParams) { .color = v4f32(1, 1, 1, 1), .radius = radius, __VA_ARGS__ })
+
 internal R_RectInstance *render_rect_(Renderer *renderer, Vec2F32 min, Vec2F32 max, R_RectParams *params);
 
 internal Void render_push_clip(Renderer *renderer, Vec2F32 min, Vec2F32 max, B32 clip_to_parent);
