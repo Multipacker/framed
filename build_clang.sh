83a91841fc69f008186ec9c8ca2b1e5d1fe0df6f
Multipacker <simon.renhult@hotmail.se>
2023-12-17 11:45:21 +0100

    Dev: Ignore repeated initialization of struct fields in clang and gcc.

--- build_clang.sh
+++ build_clang.sh
@@ -5,7 +5,7 @@ mode=${1:-debug}
 mkdir -p build
 
 src_files="src/main.c"
-errors="-Werror -Wall -Wno-missing-braces -Wno-newline-eof -Wno-unused-variable -Wno-unused-function -Wconversion"
+errors="-Werror -Wall -Wno-missing-braces -Wno-newline-eof -Wno-unused-variable -Wno-unused-function -Wconversion -Wno-initializer-overrides"
 common_flags="-Isrc -Ivendor -o build/out -DRENDERER_OPENGL=1"
 linker_flags="-lSDL2"
 
