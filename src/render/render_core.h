280f1b079848cc62e5e42324dfb9623e40561a60
Cr0wn24 <hampan2003@live.se>
2023-12-19 17:21:53 +0100

    Dev: Added functions to update a texture

--- src/render/render_core.h
+++ src/render/render_core.h
@@ -96,6 +96,7 @@ internal R_Texture      render_create_texture_from_bitmap(R_Context *renderer, V
 internal Void           render_destroy_texture(R_Context *renderer, R_Texture texture);
 internal R_TextureSlice render_slice_from_texture(R_Texture texture, RectF32 region);
 internal R_TextureSlice render_create_texture_slice(R_Context *renderer, Str8 path, R_ColorSpace color_space);
+internal R_Texture render_update_texture(R_Context *renderer, R_Texture texture, Void *memory, );
 
 internal F32     f32_srgb_to_linear(F32 value);
 internal Vec4F32 vec4f32_srgb_to_linear(Vec4F32 srgb);
