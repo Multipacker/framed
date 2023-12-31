3fa4f2c22b11f9b5e7313525fa95d736ae822205
Cr0wn24 <hampan2003@live.se>
2023-12-31 09:37:18 +0100

    Bugfix: D3D11 batching with different clip rects logic was wrong

--- src/render/d3d11/d3d11_render.c
+++ src/render/d3d11/d3d11_render.c
@@ -23,12 +23,12 @@ internal R_BackendContext *
 render_backend_init(R_Context *renderer)
 {
 	Gfx_Context *gfx = renderer->gfx;
-	
+
 	renderer->backend = push_struct(renderer->permanent_arena, R_BackendContext);
 	R_BackendContext *backend = renderer->backend;
-	
+
 	HRESULT hr;
-	
+
 	IDXGIInfoQueue      *dxgi_info;
 	IDXGIDevice         *dxgi_device;
 	IDXGIAdapter        *dxgi_adapter;
@@ -45,7 +45,7 @@ render_backend_init(R_Context *renderer)
 							   D3D11_SDK_VERSION, &backend->device, 0, &backend->context);
 		assert_hr(hr);
 	}
-	
+
 	// NOTE(hampus): Enable useful debug break on errors
 #if !BUILD_MODE_RELEASE
 	{
@@ -54,7 +54,7 @@ render_backend_init(R_Context *renderer)
 		ID3D11InfoQueue_SetBreakOnSeverity(info, D3D11_MESSAGE_SEVERITY_ERROR, TRUE);
 		ID3D11InfoQueue_Release(info);
 	}
-	
+
 	{
 		hr = DXGIGetDebugInterface1(0, &IID_IDXGIInfoQueue, (Void**) &dxgi_info);
 		IDXGIInfoQueue_SetBreakOnSeverity(dxgi_info, DXGI_DEBUG_ALL, DXGI_INFO_QUEUE_MESSAGE_SEVERITY_CORRUPTION, TRUE);
@@ -62,18 +62,18 @@ render_backend_init(R_Context *renderer)
 		IDXGIInfoQueue_Release(dxgi_info);
 	}
 #endif
-	
+
 	// NOTE(hampus): Create swap chain
 	{
 		hr = ID3D11Device_QueryInterface(backend->device, &IID_IDXGIDevice, (Void**) &dxgi_device);
 		assert_hr(hr);
-		
+
 		hr = IDXGIDevice_GetAdapter(dxgi_device, &dxgi_adapter);
 		assert_hr(hr);
-		
+
 		hr = IDXGIAdapter_GetParent(dxgi_adapter, &IID_IDXGIFactory2, (Void**) &factory);
 		assert_hr(hr);
-		
+
 		DXGI_SWAP_CHAIN_DESC1 desc =
 		{
 			//.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB,
@@ -82,22 +82,22 @@ render_backend_init(R_Context *renderer)
 			.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT,
 			.BufferCount = 2,
 			.Scaling = DXGI_SCALING_NONE,
-			
+
 			// NOTE(hampus): for Windows 8 compatibility use DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL
 			// NOTE(hampus): for Windows 7 compatibility use DXGI_SWAP_EFFECT_DISCARD
 			.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD,
 		};
-		
+
 		hr = IDXGIFactory2_CreateSwapChainForHwnd(factory, (IUnknown*) backend->device, gfx->hwnd, &desc, 0, 0, &backend->swap_chain);
 		assert_hr(hr);
-		
+
 		IDXGIFactory_MakeWindowAssociation(factory, gfx->hwnd, DXGI_MWA_NO_ALT_ENTER);
-		
+
 		IDXGIFactory2_Release(factory);
 		IDXGIAdapter_Release(dxgi_adapter);
 		IDXGIDevice_Release(dxgi_device);
 	}
-	
+
 	// NOTE(hampus): Create vertex buffer
 	{
 		D3D11_BUFFER_DESC desc =
@@ -107,68 +107,68 @@ render_backend_init(R_Context *renderer)
 			.BindFlags = D3D11_BIND_VERTEX_BUFFER,
 			.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE
 		};
-		
+
 		ID3D11Device_CreateBuffer(backend->device, &desc, 0, &backend->vertex_buffer);
 	}
-	
+
 	// NOTE(hampus): Create shaders
 	{
 		D3D11_INPUT_ELEMENT_DESC desc[] =
 		{
 			{ "MIN", 0, DXGI_FORMAT_R32G32_FLOAT, 0,
 				member_offset(R_RectInstance, min), D3D11_INPUT_PER_INSTANCE_DATA, 1 },
-			
+
 			{ "MAX", 0, DXGI_FORMAT_R32G32_FLOAT, 0,
 				member_offset(R_RectInstance, max), D3D11_INPUT_PER_INSTANCE_DATA, 1 },
-			
+
 			{ "MIN_UV", 0, DXGI_FORMAT_R32G32_FLOAT, 0,
 				member_offset(R_RectInstance, min_uv), D3D11_INPUT_PER_INSTANCE_DATA, 1 },
-			
+
 			{ "MAX_UV", 0, DXGI_FORMAT_R32G32_FLOAT, 0,
 				member_offset(R_RectInstance, max_uv), D3D11_INPUT_PER_INSTANCE_DATA, 1 },
-			
+
 			{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0,
 				member_offset(R_RectInstance, colors), D3D11_INPUT_PER_INSTANCE_DATA, 1 },
-			
+
 			{ "COLOR", 1, DXGI_FORMAT_R32G32B32A32_FLOAT, 0,
 				member_offset(R_RectInstance, colors)+sizeof(Vec4F32), D3D11_INPUT_PER_INSTANCE_DATA, 1 },
-			
+
 			{ "COLOR", 2, DXGI_FORMAT_R32G32B32A32_FLOAT, 0,
 				member_offset(R_RectInstance, colors)+sizeof(Vec4F32)*2, D3D11_INPUT_PER_INSTANCE_DATA, 1 },
-			
+
 			{ "COLOR", 3, DXGI_FORMAT_R32G32B32A32_FLOAT, 0,
 				member_offset(R_RectInstance, colors)+sizeof(Vec4F32)*3, D3D11_INPUT_PER_INSTANCE_DATA, 1 },
-			
+
 			{ "CORNER_RADIUS", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0,
 				member_offset(R_RectInstance, radies), D3D11_INPUT_PER_INSTANCE_DATA, 1 },
-			
+
 			{ "SOFTNESS", 0, DXGI_FORMAT_R32_FLOAT, 0,
 				member_offset(R_RectInstance, softness), D3D11_INPUT_PER_INSTANCE_DATA, 1 },
-			
+
 			{ "BORDER_THICKNESS", 0, DXGI_FORMAT_R32_FLOAT, 0,
 				member_offset(R_RectInstance, border_thickness), D3D11_INPUT_PER_INSTANCE_DATA, 1 },
-			
+
 			{ "OMIT_TEXTURE", 0, DXGI_FORMAT_R32_FLOAT, 0,
 				member_offset(R_RectInstance, omit_texture), D3D11_INPUT_PER_INSTANCE_DATA, 1 },
-			
+
 			{ "IS_SUBPIXEL_TEXT", 0, DXGI_FORMAT_R32_FLOAT, 0,
 				member_offset(R_RectInstance, is_subpixel_text), D3D11_INPUT_PER_INSTANCE_DATA, 1 },
-			
+
 		};
-		
+
 		UINT flags = D3DCOMPILE_PACK_MATRIX_COLUMN_MAJOR | D3DCOMPILE_ENABLE_STRICTNESS | D3DCOMPILE_WARNINGS_ARE_ERRORS;
 #if !BUILD_MODE_RELEASE
 		flags |= D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
 #else
 		flags |= D3DCOMPILE_OPTIMIZATION_LEVEL3;
 #endif
-		
+
 		Arena_Temporary scratch = get_scratch(0, 0);
-		
+
 		Str8 hlsl = { 0 };
 		B32 hlsl_file_read_result = os_file_read(scratch.arena, str8_lit("src/render/d3d11/d3d11_shader.hlsl"), &hlsl);
 		assert(hlsl_file_read_result);
-		
+
 		ID3DBlob* error;
 		ID3DBlob* vblob;
 		hr = D3DCompile(hlsl.data, hlsl.size, 0, 0, 0, "vs", "vs_5_0", flags, 0, &vblob, &error);
@@ -178,7 +178,7 @@ render_backend_init(R_Context *renderer)
 			OutputDebugStringA(message);
 			assert(!"Failed to compile vertex shader!");
 		}
-		
+
 		ID3DBlob* pblob;
 		hr = D3DCompile(hlsl.data, hlsl.size, 0, 0, 0, "ps", "ps_5_0", flags, 0, &pblob, &error);
 		if (FAILED(hr))
@@ -187,16 +187,16 @@ render_backend_init(R_Context *renderer)
 			OutputDebugStringA(message);
 			assert(!"Failed to compile pixel shader!");
 		}
-		
+
 		ID3D11Device_CreateVertexShader(backend->device, ID3D10Blob_GetBufferPointer(vblob), ID3D10Blob_GetBufferSize(vblob), 0, &backend->vertex_shader);
 		ID3D11Device_CreatePixelShader(backend->device, ID3D10Blob_GetBufferPointer(pblob), ID3D10Blob_GetBufferSize(pblob), 0, &backend->pixel_shader);
 		ID3D11Device_CreateInputLayout(backend->device, desc, ARRAYSIZE(desc), ID3D10Blob_GetBufferPointer(vblob), ID3D10Blob_GetBufferSize(vblob), &backend->input_layout);
 		ID3D10Blob_Release(pblob);
 		ID3D10Blob_Release(vblob);
-		
+
 		release_scratch(scratch);
 	}
-	
+
 	{
 		D3D11_BUFFER_DESC desc =
 		{
@@ -207,14 +207,14 @@ render_backend_init(R_Context *renderer)
 		};
 		ID3D11Device_CreateBuffer(backend->device, &desc, 0, &backend->uniform_buffer);
 	}
-	
+
 	// NOTE(hampus): White Texture
 	{
 		U32 white = 0xffffffff;
-		
+
 		UINT width = 1;
 		UINT height = 1;
-		
+
 		D3D11_TEXTURE2D_DESC desc =
 		{
 			.Width = width,
@@ -226,22 +226,22 @@ render_backend_init(R_Context *renderer)
 			.Usage = D3D11_USAGE_IMMUTABLE,
 			.BindFlags = D3D11_BIND_SHADER_RESOURCE,
 		};
-		
+
 		D3D11_SUBRESOURCE_DATA data =
 		{
 			.pSysMem = &white,
 			.SysMemPitch = width * sizeof(U32),
 		};
-		
+
 		ID3D11Texture2D *texture;
 		ID3D11ShaderResourceView *texture_view;
 		ID3D11Device_CreateTexture2D(backend->device, &desc, &data, &texture);
 		ID3D11Device_CreateShaderResourceView(backend->device, (ID3D11Resource*) texture, 0, &texture_view);
 		ID3D11Texture2D_Release(texture);
-		
+
 		backend->white_texture.u64[0] = int_from_ptr(texture_view);
 	}
-	
+
 	// NOTE(hampus): Sampler
 	{
 		D3D11_SAMPLER_DESC desc =
@@ -254,10 +254,10 @@ render_backend_init(R_Context *renderer)
 			.MinLOD = -F32_MAX,
 			.MaxLOD = +F32_MAX,
 		};
-		
+
 		ID3D11Device_CreateSamplerState(backend->device, &desc, &backend->sampler);
 	}
-	
+
 	// NOTE(hampus): Blend state
 	{
 		// NOTE(hampus): Enable alpha blending
@@ -266,27 +266,27 @@ render_backend_init(R_Context *renderer)
 			.RenderTarget[0] =
 			{
 				.BlendEnable = TRUE,
-				
+
 				// NOTE(hampus): For subpixel text rendering the rgb values are acting
 				// as an alpha value for each component. Therefore we specify another
 				// color source which will be these alpha values.
-				
+
 				// NOTE(hampus): dst.rgb = dst.rgb * (1 - src1.rgb) + src0.rgb * (src1.rgb)
 				.SrcBlend = D3D11_BLEND_SRC1_COLOR,
 				.DestBlend = D3D11_BLEND_INV_SRC1_COLOR,
 				.BlendOp = D3D11_BLEND_OP_ADD,
-				
+
 				// NOTE(hampus): dst.a = dst.a * (1 - color.a) + src.a * (color.a)
 				.SrcBlendAlpha = D3D11_BLEND_SRC_ALPHA,
 				.DestBlendAlpha = D3D11_BLEND_INV_SRC_ALPHA,
 				.BlendOpAlpha = D3D11_BLEND_OP_ADD,
-				
+
 				.RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL,
 			},
 		};
 		ID3D11Device_CreateBlendState(backend->device, &desc, &backend->blend_state);
 	}
-	
+
 	// NOTE(hampus): Rasterizer state
 	{
 		// NOTE(hampus): Disable culling
@@ -298,7 +298,7 @@ render_backend_init(R_Context *renderer)
 		};
 		ID3D11Device_CreateRasterizerState(backend->device, &desc, &backend->rasterizer_state);
 	}
-	
+
 	// NOTE(hampus): Depth stencil state
 	{
 		// NOTE(hampus): Disable depth & stencil test
@@ -313,7 +313,7 @@ render_backend_init(R_Context *renderer)
 		};
 		ID3D11Device_CreateDepthStencilState(backend->device, &desc, &backend->depth_state);
 	}
-	
+
 	return(backend);
 }
 
@@ -339,7 +339,7 @@ render_backend_begin(R_Context *renderer)
 	max_clip.x = (F32) client_area.x;
 	max_clip.y = (F32) client_area.y;
 	render_push_clip(renderer, v2f32(0, 0), max_clip, false);
-	
+
 	// NOTE(hampus): First batch
 	D3D11_Batch *first_batch = d3d11_push_batch(renderer);
 	first_batch->params.texture = renderer->backend->white_texture;
@@ -360,7 +360,7 @@ render_backend_end(R_Context *renderer)
 			ID3D11DepthStencilView_Release(backend->depth_stencil_view);
 			backend->render_target_view = 0;
 		}
-		
+
 		if (client_area.width != 0 && client_area.height != 0)
 		{
 			hr = IDXGISwapChain1_ResizeBuffers(backend->swap_chain, 0, client_area.width, client_area.height, DXGI_FORMAT_UNKNOWN, 0);
@@ -368,14 +368,14 @@ render_backend_end(R_Context *renderer)
 			{
 				assert(!"Failed to resize swap chain!");
 			}
-			
+
 			// NOTE(hampus): Create RenderTarget view for new backbuffer texture
 			ID3D11Texture2D* backbuffer;
 			IDXGISwapChain1_GetBuffer(backend->swap_chain, 0, &IID_ID3D11Texture2D, (Void**) &backbuffer);
 			D3D11_RENDER_TARGET_VIEW_DESC render_target_view_desc = { .Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB, .ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D };
 			ID3D11Device_CreateRenderTargetView(backend->device, (ID3D11Resource*) backbuffer, &render_target_view_desc, &backend->render_target_view);
 			ID3D11Texture2D_Release(backbuffer);
-			
+
 			D3D11_TEXTURE2D_DESC depthDesc =
 			{
 				.Width = client_area.width,
@@ -387,7 +387,7 @@ render_backend_end(R_Context *renderer)
 				.Usage = D3D11_USAGE_DEFAULT,
 				.BindFlags = D3D11_BIND_DEPTH_STENCIL,
 			};
-			
+
 			// NOTE(hampus): Create new depth stencil texture & DepthStencil view
 			ID3D11Texture2D* depth;
 			ID3D11Device_CreateTexture2D(backend->device, &depthDesc, 0, &depth);
@@ -397,11 +397,11 @@ render_backend_end(R_Context *renderer)
 		backend->current_width = client_area.width;
 		backend->current_height = client_area.height;
 	}
-	
+
 	F32 gamma = 2.2f;
-	
+
 	Vec4F32 white = v4f32(1, 1, 1, 1);
-	
+
 	if (backend->render_target_view)
 	{
 		D3D11_VIEWPORT viewport =
@@ -413,10 +413,10 @@ render_backend_end(R_Context *renderer)
 			.MinDepth = 0,
 			.MaxDepth = 1,
 		};
-		
+
 		R_RenderStats *stats = d3d11_get_current_stats(renderer);
 		Vec4F32 bg_color = vec4f32_srgb_to_linear(v4f32(0.1f, 0.2f, 0.3f, 1.f));
-		
+
 		FLOAT color[] = { bg_color.r, bg_color.g, bg_color.b, bg_color.a };
 		ID3D11DeviceContext_ClearRenderTargetView(backend->context, backend->render_target_view, color);
 		ID3D11DeviceContext_ClearDepthStencilView(backend->context, backend->depth_stencil_view, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.f, 0);
@@ -426,17 +426,17 @@ render_backend_end(R_Context *renderer)
 			 batch = batch->next)
 		{
 			D3D11_BatchParams *params = &batch->params;
-			
+
 			// NOTE(hampus): Setup uniform buffer
 			{
 				Mat4F32 transform = m4f32_ortho(0, (F32) client_area.width, (F32) client_area.height, 0, -1.0f, 1.0f);
-				
+
 				D3D11_MAPPED_SUBRESOURCE mapped;
 				ID3D11DeviceContext_Map(backend->context, (ID3D11Resource*) backend->uniform_buffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mapped);
 				memory_copy_typed((U8 *) mapped.pData, &transform.m, sizeof(transform));
 				ID3D11DeviceContext_Unmap(backend->context, (ID3D11Resource*) backend->uniform_buffer, 0);
 			}
-			
+
 			// NOTE(hampus): Setup vertex buffer
 			{
 				D3D11_MAPPED_SUBRESOURCE mapped;
@@ -444,47 +444,47 @@ render_backend_end(R_Context *renderer)
 				memory_copy_typed((U8 *) mapped.pData, (U8 *) batch->instances, sizeof(R_RectInstance) * batch->instance_count);
 				ID3D11DeviceContext_Unmap(backend->context, (ID3D11Resource*) backend->vertex_buffer, 0);
 			}
-			
+
 			// NOTE(hampus): Input Assembler
 			ID3D11DeviceContext_IASetInputLayout(backend->context, backend->input_layout);
 			ID3D11DeviceContext_IASetPrimitiveTopology(backend->context, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
 			UINT stride = sizeof(R_RectInstance);
 			UINT offset = 0;
 			ID3D11DeviceContext_IASetVertexBuffers(backend->context, 0, 1, &backend->vertex_buffer, &stride, &offset);
-			
+
 			// NOTE(hampus): Vertex Shader
 			ID3D11DeviceContext_VSSetConstantBuffers(backend->context, 0, 1, &backend->uniform_buffer);
 			ID3D11DeviceContext_VSSetShader(backend->context, backend->vertex_shader, 0, 0);
-			
+
 			D3D11_RECT rect;
 			rect.left   = (LONG) params->clip_rect->rect.x0;
 			rect.top    = (LONG) params->clip_rect->rect.y0;
 			rect.right  = (LONG) params->clip_rect->rect.x1;
 			rect.bottom = (LONG) params->clip_rect->rect.y1;
-			
+
 			// NOTE(hampus): Rasterizer Stage
 			ID3D11DeviceContext_RSSetViewports(backend->context, 1, &viewport);
 			ID3D11DeviceContext_RSSetState(backend->context, backend->rasterizer_state);
 			ID3D11DeviceContext_RSSetScissorRects(backend->context, 1, &rect);
-			
+
 			ID3D11ShaderResourceView *texture_view = ptr_from_int(batch->params.texture.u64[0]);
-			
+
 			// NOTE(hampus): Pixel Shader
 			ID3D11DeviceContext_PSSetSamplers(backend->context, 0, 1, &backend->sampler);
 			ID3D11DeviceContext_PSSetShaderResources(backend->context, 0, 1, &texture_view);
 			ID3D11DeviceContext_PSSetShader(backend->context, backend->pixel_shader, 0, 0);
-			
+
 			// NOTE(hampus): Output Merger
 			ID3D11DeviceContext_OMSetBlendState(backend->context, backend->blend_state, 0, ~0U);
 			ID3D11DeviceContext_OMSetDepthStencilState(backend->context, backend->depth_state, 0);
 			ID3D11DeviceContext_OMSetRenderTargets(backend->context, 1, &backend->render_target_view, backend->depth_stencil_view);
-			
+
 			// NOTE(hampus): Draw
 			assert(batch->instance_count <= U32_MAX);
 			ID3D11DeviceContext_DrawInstanced(backend->context, 4, (U32) batch->instance_count, 0, 0);
 		}
 	}
-	
+
 	BOOL vsync = TRUE;
 	hr = IDXGISwapChain1_Present(backend->swap_chain, vsync ? 1 : 0, 0);
 	if (hr == DXGI_STATUS_OCCLUDED)
@@ -499,7 +499,7 @@ render_backend_end(R_Context *renderer)
 	{
 		assert(!"Failed to present swap chain! Device lost?");
 	}
-	
+
 	// NOTE(hampus): Reset state
 	render_pop_clip(renderer);
 	swap(renderer->render_stats[0], renderer->render_stats[1], R_RenderStats);
@@ -519,29 +519,29 @@ render_rect_(R_Context *renderer, Vec2F32 min, Vec2F32 max, R_RectParams *params
 	}
 	D3D11_BatchList *batch_list = &backend->batch_list;
 	D3D11_Batch *batch = batch_list->last;
-	
+
 	R_RectInstance *instance = &render_rect_instance_null;
-	
+
 	// NOTE(simon): Account for softness.
 	RectF32 expanded_area = rectf32(
 									v2f32_sub_f32(min, params->softness),
 									v2f32_add_f32(max, params->softness)
 									);
-	
+
 	if (!rectf32_overlaps(expanded_area, d3d11_top_clip(renderer)->rect))
 	{
 		return(instance);
 	}
-	
+
 	B32 is_different_clip   = (batch->params.clip_rect != backend->clip_rect_stack.first);
 	B32 inside_current_clip = rectf32_contains_rectf32(d3d11_top_clip(renderer)->rect, expanded_area);
-	B32 inside_batch_clip   = rectf32_contains_rectf32(d3d11_top_clip(renderer)->rect, expanded_area);
+	B32 inside_batch_clip   = rectf32_contains_rectf32(batch->params.clip_rect->rect, expanded_area);
 	if ((is_different_clip && !(inside_current_clip && inside_batch_clip)) ||
 		batch->instance_count >= D3D11_BATCH_SIZE)
 	{
 		batch = 0;
 	}
-	
+
 	if (batch)
 	{
 		B32 batch_is_white  = batch->params.texture.u64[0] == backend->white_texture.u64[0];
@@ -565,15 +565,15 @@ render_rect_(R_Context *renderer, Vec2F32 min, Vec2F32 max, R_RectParams *params
 			}
 		}
 	}
-	
+
 	if (!batch)
 	{
 		batch = d3d11_push_batch(renderer);
 		batch->params.texture = params->slice.texture;
 	}
-	
+
 	instance = batch->instances + batch->instance_count;
-	
+
 	instance->min              = min;
 	instance->max              = max;
 	instance->min_uv           = params->slice.region.min;
@@ -590,12 +590,12 @@ render_rect_(R_Context *renderer, Vec2F32 min, Vec2F32 max, R_RectParams *params
 	instance->border_thickness = params->border_thickness;
 	instance->omit_texture     = (F32) (params->slice.texture.u64[0] == backend->white_texture.u64[0]);
 	instance->is_subpixel_text = (F32) params->is_subpixel_text;
-	
+
 	batch->instance_count++;
-	
+
 	R_RenderStats *stats = d3d11_get_current_stats(renderer);
 	stats->rect_count++;
-	
+
 	return(instance);
 }
 
@@ -642,7 +642,7 @@ render_create_texture_from_bitmap(R_Context *renderer, Void *memory, U32 width, 
 		case R_ColorSpace_Linear: d3d11_format = DXGI_FORMAT_R8G8B8A8_UNORM; break;
 		invalid_case;
 	}
-	
+
 	D3D11_TEXTURE2D_DESC desc =
 	{
 		.Width = width,
@@ -655,18 +655,18 @@ render_create_texture_from_bitmap(R_Context *renderer, Void *memory, U32 width, 
 		.BindFlags = D3D11_BIND_SHADER_RESOURCE,
 		.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE,
 	};
-	
+
 	D3D11_SUBRESOURCE_DATA data =
 	{
 		.pSysMem = memory,
 		.SysMemPitch = width * sizeof(U32),
 	};
-	
+
 	ID3D11Texture2D *texture;
 	ID3D11ShaderResourceView *texture_view;
 	ID3D11Device_CreateTexture2D(renderer->backend->device, &desc, &data, &texture);
 	ID3D11Device_CreateShaderResourceView(renderer->backend->device, (ID3D11Resource*) texture, 0, &texture_view);
-	
+
 	result.u64[0] = int_from_ptr(texture_view);
 	result.u64[1] = (U64) width;
 	result.u64[2] = (U64) height;
