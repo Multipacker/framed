a4919f9a85b8421d772181c873b755db7193f685
Simon Renhult <simon.renhult@hotmail.se>
2024-01-01 22:05:49 +0100

    Dev: Implemented `atomic_compare_exhange` on clang and gcc.

--- src/base/base_atomic.c
+++ src/base/base_atomic.c
@@ -16,17 +16,20 @@ u32_atomic_add(U32 volatile *location, U32 amount)
 	return(result);
 }
 
+// TODO(simon): This function should ONLY perform a compare exchange and
+// nothing else to be usefull in as many places as possible.
 internal B32
 u32_atomic_compare_exchange(U32 volatile *location, U32 exchange_value, U32 compare_value)
 {
 	// TODO(simon): Is the memory ordering correct?
 #if COMPILER_CLANG || COMPILER_GCC
-#error "There isn't a u32_atomic_add for your compiler."
+	U32 initial_value = compare_value;
+	__atomic_compare_exchange_n(location, &initial_value, exchange_value, false, memory_order_acq_rel, memory_order_acquire);
 #elif COMPILER_CL
 	// NOTE(hampus): Returns the value at location
 	U32 initial_value = InterlockedCompareExchange((LONG *)location, exchange_value, compare_value);
 #else
-#error "There isn't a u32_atomic_add for your compiler."
+#error "There isn't a u32_atomic_compare_exchange for your compiler."
 #endif
 	return(initial_value == compare_value);
 }
