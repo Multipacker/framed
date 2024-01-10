e21b6a293d8df487a9dcbb114e87a500b3361d7a
Multipacker <simon.renhult@hotmail.se>
2024-01-10 22:02:08 +0100

    Dev(image): Added tables for decoding backwards references.

--- src/image/image_inc.c
+++ src/image/image_inc.c
@@ -49,6 +49,39 @@ global U32 png_linear_alphabet[] = {
 	256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268, 269, 270, 271,
 	272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285,
 };
+
+// TODO(simon): Maybe we should store dummy value until index 257. Profile!
+// NOTE(simon): Base index is 257
+global U32 png_length_extra_bits[] = {
+	0, 0, 0, 0, 0, 0, 0, 0,
+	1, 1, 1, 1, 2, 2, 2, 2,
+	3, 3, 3, 3, 4, 4, 4, 4,
+	5, 5, 5, 5, 0,
+};
+
+// TODO(simon): Maybe we should store dummy value until index 257. Profile!
+// NOTE(simon): Base index is 257
+global U32 png_length_base[] = {
+	3,   4,   5,   6,   7,  8,  9,  10,
+	11,  13,  15,  17,  19, 23, 27, 31,
+	35,  43,  51,  59,  67, 83, 99, 115,
+	131, 163, 195, 227, 258,
+};
+
+global U32 png_distance_extra_bits[] = {
+	0,  0,  0,  0,  1,  1, 2,  2,
+	3,  3,  4,  4,  5,  5, 6,  6,
+	7,  7,  8,  8,  9,  9, 10, 10,
+	11, 11, 12, 12, 13, 13,
+};
+
+global U32 png_distance_base[] = {
+	1,    2,    3,    4,     5,     7,    9,    13,
+	17,   25,   33,   49,    65,    97,   129,  193,
+	257,  385,  513,  769,   1025,  1537, 2049, 3073,
+	4097, 6145, 8193, 12289, 16385, 24577,
+};
+
 typedef struct PNG_Chunk PNG_Chunk;
 struct PNG_Chunk
 {
