b5f02949610233677ff11434d6293bf23c63407b
Multipacker <simon.renhult@hotmail.se>
2024-01-17 09:48:41 +0100

    Dev(script): Added some size optimizations for release builds with clang and gcc.

--- script/build_clang.sh
+++ script/build_clang.sh
@@ -5,8 +5,9 @@ mode=${1:-debug}
 mkdir -p build
 
 src_files="src/profiler/profiler_main.c"
+out_file="build/out"
 errors="-Werror -Wall -Wno-missing-braces -Wno-newline-eof -Wno-unused-variable -Wno-unused-function -Wconversion -Wno-initializer-overrides -Wno-unused-but-set-variable -Wshadow -Wsign-compare -Wsign-conversion -Wtautological-compare -Wtype-limits"
-common_flags="-Isrc -Ivendor -o build/out -DRENDERER_OPENGL=1 -pthread"
+common_flags="-Isrc -Ivendor -o $out_file -DRENDERER_OPENGL=1 -pthread"
 linker_flags="-fuse-ld=mold -lSDL2 build/freetype/freetype -lm"
 
 if [ "$mode" == "debug" ]; then
@@ -14,10 +15,14 @@ if [ "$mode" == "debug" ]; then
 elif [ "$mode" == "optimized" ]; then
 	compiler_flags="-g -O3 -march=native -fsanitize=address -DENABLE_ASSERT=1 -DBUILD_MODE_OPTIMIZED=1"
 elif [ "$mode" == "release" ]; then
-	compiler_flags="-O3 -march=native -DBUILD_MODE_RELEASE=1"
+	compiler_flags="-O3 -s -march=native -DBUILD_MODE_RELEASE=1 -Wl,--gc-sections -fdata-sections -fno-stack-protector -fno-unwind-tables -fno-asynchronous-unwind-tables -Wa,--noexecstack"
 else
 	echo "ERROR: Unknown build type."
 	exit 1
 fi
 
 clang $src_files $errors $common_flags $compiler_flags $linker_flags
+
+if [ "$mode" == "release" ]; then
+	strip -R .comment $out_file
+fi
