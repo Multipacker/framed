61417814a08dda777f6887329aa06fab6024982f
Multipacker <simon.renhult@hotmail.se>
2024-02-15 23:08:12 +0100

    Dev(script): Change Clang and GCC to produce executables named `framed`.

--- script/build_clang.sh
+++ script/build_clang.sh
@@ -5,7 +5,7 @@ mode=${1:-debug}
 mkdir -p build
 
 src_files="src/framed/framed_main.c"
-out_file="build/out"
+out_file="build/framed"
 errors="-Werror -Wall -Wno-missing-braces -Wno-newline-eof -Wno-unused-variable -Wno-unused-function -Wconversion -Wno-initializer-overrides -Wno-unused-but-set-variable -Wshadow -Wsign-compare -Wsign-conversion -Wtautological-compare -Wtype-limits"
 common_flags="-Isrc -Ivendor -o $out_file -DRENDERER_OPENGL=1 -pthread"
 linker_flags="-lSDL2 build/freetype/freetype -lm"
