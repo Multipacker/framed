e4f9b5ff875b4d76bb7f5b3289c389547a9e2a9d
Cr0wn24 <hampan2003@live.se>
2024-02-21 15:24:04 +0100

    Dev(Script): Output to framed.exe

--- script/build_msvc.bat
+++ script/build_msvc.bat
@@ -17,7 +17,7 @@
 set disabled_warnings=-wd4201 -wd4152 -wd4100 -wd4189 -wd4101 -wd4310 -wd4061 -wd4820 -wd4191 -wd5045 -wd4711 -wd4710 -wd4200
 set additional_includes=-I../vendor/ -I../vendor/freetype/include -I../src/ -I../
 set opts=-DENABLE_ASSERT=1 -DRENDERER_D3D11=1
-set compiler_flags=%opts% -nologo -FC -Wall -MP -WX %disabled_warnings% %additional_includes% -Fe:main
+set compiler_flags=%opts% -nologo -FC -Wall -MP -WX %disabled_warnings% %additional_includes% -Fe:framed
 set libs=user32.lib kernel32.lib winmm.lib gdi32.lib shcore.lib
 set linker_flags=%libs% -incremental:no
 set src_files=../src/framed/framed_main.c
@@ -70,7 +70,7 @@ if %build_mode% == "debug" (
 if not exist build mkdir build
 pushd build
 
-del *.pdb
+if exist *.pdb del *.pdb
 
 cl %src_files% %compiler_flags% -link %linker_flags%
 
