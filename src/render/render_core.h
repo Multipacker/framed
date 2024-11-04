c7c7107db8bcbbe1332b8a10e031baa1f6c4b8fd
Simon Renhult <simon.renhult@hotmail.se>
2024-11-05 00:49:40 +0100

    Cleanup(render): Added missing forward declaration for `render_pop_clip`.

--- src/render/render_core.h
+++ src/render/render_core.h
@@ -120,7 +120,10 @@ internal F32     f32_linear_to_srgb(F32 value);
 internal Vec4F32 vec4f32_linear_to_srgb(Vec4F32 linear);
 
 internal Render_RenderStats render_get_stats(Render_Context *renderer);
+
 internal Void render_push_clip(Render_Context *renderer, Vec2F32 min, Vec2F32 max, B32 clip_to_parent);
+internal Void render_pop_clip(Render_Context *renderer);
+
 internal Render_Context *render_init(Void);
 internal Void render_begin(Render_Context *renderer);
 internal Void render_end(Render_Context *renderer);
